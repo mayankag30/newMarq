4b3111a321b70fdc64f235c51a714b9d44ac481c
MAYANK AGARWAL <63557923+mayankag30@users.noreply.github.com>
2021-05-15 23:05:39 +0530

    Delete src directory

--- src/pages/About.js
+++
@@ -1,8 +0,0 @@
-import { Route, Link } from "react-router-dom";
-import AboutUsPage from "../components/ui/AboutUsPage/AboutUsPage";
-
-const About = () => {
-  return <AboutUsPage />;
-};
-
-export default About;
